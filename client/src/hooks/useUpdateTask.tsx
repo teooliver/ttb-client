0a81f915a127a1675e52ea5bcf5eac823ad1f861
Teo Oliver <teo.liz.oli@gmail.com>
2021-12-02 23:52:23 +0000

    fix: now task initial and end times can be updated

--- client/src/hooks/useUpdateTask.tsx
+++ client/src/hooks/useUpdateTask.tsx
@@ -3,13 +3,21 @@ import { IDbTask } from '../interfaces/task';
 import { API_URL } from '../utils/api-client';
 
 const updateTask = async (task: Partial<IDbTask>) => {
+  const editable_fields = {
+    name: task.name,
+    initial_time: task.initial_time,
+    end_time: task.end_time,
+  };
+
+  console.log('===== Task =====', editable_fields);
+  console.log('===== TaskID =====', task._id);
   const res = await fetch(`${API_URL}/tasks/${task._id}`, {
-    method: 'PATCH',
+    method: 'PUT',
     headers: {
       Accept: 'application/json',
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify(task),
+    body: JSON.stringify(editable_fields),
   }).then((res) => res.json());
 
   return res as IDbTask;
