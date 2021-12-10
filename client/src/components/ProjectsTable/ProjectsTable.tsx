7ffb95f46c9881844f9027d0e4bd798d7d0dc91a
Teo Oliver <teo.liz.oli@gmail.com>
2021-12-10 22:28:17 +0000

    feat: delete projects

--- client/src/components/ProjectsTable/ProjectsTable.tsx
+++ client/src/components/ProjectsTable/ProjectsTable.tsx
@@ -1,9 +1,12 @@
+import { useDeleteProject } from '../../hooks/useDeleteProject';
 import { useGetProjects } from '../../hooks/useGetProjects';
 import { Dot } from '../icons/Dot';
+import { Trash } from '../icons/Trash';
 import styles from './ProjectsTable.module.css';
 
 const ProjectsTable = () => {
   const { data: projects, isLoading, isSuccess, isError } = useGetProjects();
+  const deleteProjectMutation = useDeleteProject();
 
   if (isError) {
     <div>No projects available</div>;
@@ -13,11 +16,10 @@ const ProjectsTable = () => {
     <div>Loading Projects</div>;
   }
 
-  console.log('PROjECTS', projects);
-
   return (
     <table className={styles['projects-table']}>
       <colgroup>
+        <col width='20%' />
         <col width='20%' />
         <col width='20%' />
         <col width='20%' />
@@ -29,6 +31,7 @@ const ProjectsTable = () => {
           <th>Client</th>
           <th>Status</th>
           <th>Team</th>
+          <th></th>
         </tr>
       </thead>
       <tbody className={styles['table-body']}>
@@ -48,6 +51,13 @@ const ProjectsTable = () => {
                   <td className={styles['client-name']}>{client._id}</td>
                   <td>-</td>
                   <td>-</td>
+                  <td className={styles['close']}>
+                    <span
+                      onClick={() => deleteProjectMutation.mutate(project._id)}
+                    >
+                      <Trash className={styles['trash-icon']} />
+                    </span>
+                  </td>
                 </tr>
               ))
             )
