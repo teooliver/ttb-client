246e2220f58b70cfee12ad7e9e28ecfd18cd085c
Teo Oliver <teo.liz.oli@gmail.com>
2022-01-23 20:15:18 +0000

    test: Spinner

--- client/src/components/Spinner/Spinner.tsx
+++ client/src/components/Spinner/Spinner.tsx
@@ -2,7 +2,7 @@ import styles from './Spinner.module.css';
 
 const Spinner = () => {
   return (
-    <div className={styles['spinner-container']}>
+    <div className={styles['spinner-container']} data-testid='spinner'>
       <span className={styles['loader']}></span>;
     </div>
   );
