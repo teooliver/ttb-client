2ece52c33a1ada288116a1765db0d6d9bd6b565d
Teo Oliver <teo.liz.oli@gmail.com>
2022-02-05 19:47:06 +0000

    fix: update storybook preview params

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,12 +1,12 @@
 import TaskProvider from '../src/context/TaskContext';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter } from 'react-router-dom';
-import 'react-datepicker/dist/react-datepicker.css';
-import '@reach/listbox/styles.css';
-import '@reach/dialog/styles.css';
 import '../src/styles/reset.css';
 import '../src/styles/variables.css';
 import '../src/styles/global.css';
+import 'react-datepicker/dist/react-datepicker.css';
+import '@reach/listbox/styles.css';
+import '@reach/dialog/styles.css';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
@@ -15,6 +15,15 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+  },
+  backgrounds: {
+    default: 'dark',
+    values: [
+      {
+        name: 'dark',
+        value: '#262d31',
+      },
+    ],
   },
 };
 const queryClient = new QueryClient();
--- src/components/TimeTracker/Clock/Clock.tsx
+++ src/components/TimeTracker/Clock/Clock.tsx
@@ -7,12 +7,12 @@ export interface ClockProps {
 
 export const Clock: FC<ClockProps> = ({ timerArray }) => {
   return (
-    <section className={styles['clock']}>
+    <div className={styles['clock']}>
       <span className={styles['time-text']}>{timerArray[0]}</span>
       <span>:</span>
       <span className={styles['time-text']}>{timerArray[1]}</span>
       <span>:</span>
       <span className={styles['time-text']}>{timerArray[2]}</span>
-    </section>
+    </div>
   );
 };
