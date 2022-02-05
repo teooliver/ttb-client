e7784da050041cc10c7f1e74ef27c9575e624b84
Teo Oliver <teo.liz.oli@gmail.com>
2022-02-05 20:36:57 +0000

    chore: cleanup

--- src/components/Navbar/Navbar.stories.tsx
+++ src/components/Navbar/Navbar.stories.tsx
@@ -1,35 +1,15 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import Navbar from './Navbar';
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'Navbar',
   component: Navbar,
 } as ComponentMeta<typeof Navbar>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof Navbar> = () => <Navbar />;
 
 export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
   primary: true,
   label: 'Navbar',
 };
-
-// export const Secondary = Template.bind({});
-// Secondary.args = {
-//   label: 'Button',
-// };
-
-// export const Large = Template.bind({});
-// Large.args = {
-//   size: 'large',
-//   label: 'Button',
-// };
-
-// export const Small = Template.bind({});
-// Small.args = {
-//   size: 'small',
-//   label: 'Button',
-// };
