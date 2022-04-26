5bf7e94af0e719660b66fa5d57566389787d48b5
Teo Oliver <teo.liz.oli@gmail.com>
2022-04-26 21:37:21 +0100

    fix: revert pagination code

--- src/components/TimeTracker/EventsTable/EventsTable.tsx
+++ src/components/TimeTracker/EventsTable/EventsTable.tsx
@@ -4,49 +4,55 @@ import EventRow from '../EventRow/EventRow';
 import styles from './EventsTable.module.css';
 import { useGetInifiniteTasks } from '../../../hooks/useGetInifiniteTasks';
 import Spinner from '../../Spinner/Spinner';
+import { useGetGroupedTasks } from '../../../hooks/useGetGroupedTasks';
 
 const EventsTable = () => {
+  // const {
+  //   data,
+  //   isFetchingNextPage,
+  //   fetchNextPage,
+  //   hasNextPage,
+  //   isSuccess,
+  //   isError,
+  //   isLoading,
+  // } = useGetInifiniteTasks();
   const {
-    data,
-    isFetchingNextPage,
-    fetchNextPage,
-    hasNextPage,
+    data: groupedTasks,
     isSuccess,
     isError,
     isLoading,
-  } = useGetInifiniteTasks();
+  } = useGetGroupedTasks(1);
 
   return (
     <>
       {isLoading && <Spinner />}
 
       {isSuccess &&
-        data?.pages.map((groupedTasks) =>
-          groupedTasks?.results.map((group) => {
-            const [hours, minutes, seconds] = calculateTimer(
-              Math.round(group.total_time)
-            );
-            return (
-              <ul
-                key={group._id}
-                className={styles['EventsTable']}
-                data-testid='events-table'
-              >
-                <li className={styles['date-header']}>
-                  <span>{format(new Date(group._id), 'EEE, dd LLL')}</span>
-                  <span className={styles['day-total']}>
-                    {hours}:{minutes}:{seconds}
-                  </span>
-                </li>
-                {group.tasks.map((task) => (
-                  <EventRow key={task._id} task={task} />
-                ))}
-              </ul>
-            );
-          })
-        )}
+        // data?.pages.map((groupedTasks) =>
+        groupedTasks.results.map((group) => {
+          const [hours, minutes, seconds] = calculateTimer(
+            Math.round(group.total_time)
+          );
+          return (
+            <ul
+              key={group._id}
+              className={styles['EventsTable']}
+              data-testid='events-table'
+            >
+              <li className={styles['date-header']}>
+                <span>{format(new Date(group._id), 'EEE, dd LLL')}</span>
+                <span className={styles['day-total']}>
+                  {hours}:{minutes}:{seconds}
+                </span>
+              </li>
+              {group.tasks.map((task) => (
+                <EventRow key={task._id} task={task} />
+              ))}
+            </ul>
+          );
+        })}
 
-      {isSuccess && hasNextPage && (
+      {/* {isSuccess && hasNextPage && (
         <div className={styles['load-more']}>
           <button
             className='btn btn-primary'
@@ -56,7 +62,7 @@ const EventsTable = () => {
             {isFetchingNextPage ? 'Loading more...' : 'Load More'}
           </button>
         </div>
-      )}
+      )} */}
 
       {isError && (
         <div>
