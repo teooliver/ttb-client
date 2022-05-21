ad31d4c88aeeeec0fc2673c85337ea59e0b61790
Teo Oliver <teo.liz.oli@gmail.com>
2022-05-21 01:23:28 +0100

    fix: all tests run in nextjs

--- jest.config.js
+++ jest.config.js
@@ -27,6 +27,8 @@ module.exports = {
     /* Use babel-jest to transpile tests with the next/babel preset
     https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object */
     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+    // '^.+\\.(ts|tsx)$': 'ts-jest',
+    // '^.+\\.(js)$': 'babel-jest',
   },
   transformIgnorePatterns: [
     '/node_modules/',
